fa3a62c749ba532855d8c176480d2a8c9a675c46
Bake Balga <bbaurzhanba@gmail.com>
2023-05-07 00:05:49 +0600

    add env and alias and fix index

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     entry: path.resolve(__dirname, "..", './src/index.tsx'),
@@ -43,6 +44,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "..", "./src/index.html")
         }),
+        new Dotenv(),
         // new CopyPlugin({
         //     patterns: [{from: "source", to: "dest"}]
         // })
